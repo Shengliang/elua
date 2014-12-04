efe800ea8ff524cc4c4681687c72038de2e2e6d2
dummy <dummy@server.fake>
2014-12-03 16:33:48 -0800

    added for raw_build.sh

---
+++ inc/git_version.h
@@ -0,0 +1,9 @@
+// eLua git_version definition
+
+#ifndef __GIT_VERSION_H__
+#define __GIT_VERSION_H__
+
+#define   ELUA_STR_VERSION         "dev-41a2f2b"      
+#define   ELUA_VERSION             dev-41a2f2b        
+
+#endif
